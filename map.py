a5f1dc3b55163d936fad780095e3a00ec4eddf14
Y.Furukawa <furukawa@id-s.net>
2019-05-21 11:53:25 +0900

    overrideredirectを使うと、入力欄をタップするイベントが発生しない？ため、やり方を変えました。

--- map.py
+++ map.py
@@ -146,7 +146,11 @@ class MapApp(tk.Tk):
         tk.Tk.__init__(self, *args, **kwargs)
         self.title("MAP")
         self.geometry("{}x{}".format(WINDOW_WIDTH, WINDOW_HEIGHT))
-        self.overrideredirect(True) # タイトルバー非表示
+        self.attributes('-topmost', True) # NG:タイトルバー非表示
+        try:
+            self.attributes('-type', 'splash') # タイトルバー非表示
+        except tk.TclError as e:
+            print(e) # '-type'はLinuxのみ有効なオプションなので、他で出ても問題なし
 
         style = ttk.Style()
 #         print(style.theme_names()) # ('aqua', 'clam', 'alt', 'default', 'classic')
