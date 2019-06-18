7eeb91098ef5c3f92213367413404a39a8ac8a84
Y.Furukawa <furukawa@id-s.net>
2019-06-18 16:34:38 +0900

    セットアップ手順に不足があったので、追加しました。

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ sudo apt-get install -y python-tk python-pil.imagetk >> ./logs/setup.log
 sudo apt-get install -y libzbar0 >> ./logs/setup.log
 sudo WIFI_INSTALL_CLI=False pip install wifi >> ./logs/setup.log
 sudo pip install -r requirements.txt >> ./logs/setup.log
+
+sudo modprobe bcm2835-v4l2
+echo "bcm2835-v4l2" | sudo tee -a /etc/modules
