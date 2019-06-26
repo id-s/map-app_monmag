21afff0e97f7c0e2eddb4f8836097fd4d3ad8030
Y.Furukawa <furukawa@id-s.net>
2019-06-26 14:26:10 +0900

    繰り返し実行しても問題ないよう修正しました。

--- setup.sh
+++ setup.sh
@@ -6,4 +6,8 @@ sudo WIFI_INSTALL_CLI=False pip install wifi >> ./logs/setup.log
 sudo pip install -r requirements.txt >> ./logs/setup.log
 
 sudo modprobe bcm2835-v4l2
-echo "bcm2835-v4l2" | sudo tee -a /etc/modules
+if [ -z `grep bcm2835-v4l2 /etc/modules` ]; then
+  echo | sudo tee -a /etc/modules
+  echo "# MAP" | sudo tee -a /etc/modules
+  echo "bcm2835-v4l2" | sudo tee -a /etc/modules
+fi
