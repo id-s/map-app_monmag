fe7d6427449104db0fc04cdb5cdfdfd62bd50d39
Y.Furukawa <furukawa@id-s.net>
2019-07-04 10:30:35 +0900

    [ticket:#9522] ngrokのセットアップをスクリプトに追加しました。

--- setup.sh
+++ setup.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
+# setting
+log_file=./logs/setup.log
+
+ngrok_download_file=ngrok-stable-linux-arm.zip
+ngrok_download_url=https://bin.equinox.io/c/4VmDzA7iaHb/$ngrok_download_file
+
+
+# locale
 sudo locale-gen ja_JP.UTF-8
 
-sudo apt-get install -y python-tk python-pil.imagetk >> ./logs/setup.log
-sudo apt-get install -y libzbar0 >> ./logs/setup.log
-sudo WIFI_INSTALL_CLI=False pip install wifi >> ./logs/setup.log
-sudo pip install -r requirements.txt >> ./logs/setup.log
 
+# install
+sudo apt-get install -y python-tk python-pil.imagetk >> $log_file
+sudo apt-get install -y libzbar0 >> $log_file
+sudo WIFI_INSTALL_CLI=False pip install wifi >> $log_file
+sudo pip install -r requirements.txt >> $log_file
+
+wget $ngrok_download_url >> $log_file
+sudo unzip $ngrok_download_file -d /usr/local/bin >> $log_file
+ngrok version >> $log_file
+
+
+# module
 sudo modprobe bcm2835-v4l2
 if [ -z `grep bcm2835-v4l2 /etc/modules` ]; then
   echo | sudo tee -a /etc/modules
   echo "# MAP" | sudo tee -a /etc/modules
   echo "bcm2835-v4l2" | sudo tee -a /etc/modules
 fi
+
--- startup.sh
+++ startup.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-cd ~/Git/map-app_monmag
-
+# setting
 ymd=`date +"%Y%m%d"`
 log_file=logs/app_$ymd.log
+
+ngrok_authtoken=6cmuFXybMEhdbwYV7e5i1_5yZA1EDSTLfxGqTUTphdj
+
+
+cd ~/Git/map-app_monmag
+
 
 # update
 git pull >> $log_file
 sudo pip install -r requirements.txt >> $log_file
+
+
+# ngrok
+ngrok authtoken $ngrok_authtoken >> $log_file
+
 
 sudo python map.py >> $log_file
 
