7ce28837591484fa2b4b3a43d21317e38343c43a
Y.Furukawa <furukawa@id-s.net>
2019-08-07 17:37:59 +0900

    [FIXED] 文章がページに収まっていなかったため、分割位置を変更しました。

--- map.py
+++ map.py
@@ -646,8 +646,6 @@ class Policy1(tk.Frame):
 
             事業者名：マギー株式会社
             個人情報保護管理者：代表取締役社長 098-951-0915
-
-            入力された情報は、本目的のみに利用いたします。
             """
         text_label = tk.Label(self, text=textwrap.dedent(text), font=style.body_font,
                               wraplength=(WINDOW_WIDTH - style.padding * 2), justify="left", height=8, padx=style.padding)
@@ -697,6 +695,7 @@ class Policy2(tk.Frame):
 
         text = """
             （続き）
+            入力された情報は、本目的のみに利用いたします。
             入力された情報の第三者提供は行いません。本事業の運用業務を他社に委託する場合があります。
             情報のご提供は任意です。ご提供いただけない場合、ポイント付与通知・MyShopサービスへの入会案内メッセージはお送りいたしません。
             """
